3a67e52073766a6316f8191460e5ad6b766db6f0
ahgmAhgm <109125259+ahgmAhgm@users.noreply.github.com>
2022-08-31 09:28:27 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 @echo off
 move "%temp%\Clock.exe" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
-timeout 1 >nul
+timeout 3 >nul
 start %appdata%\Microsoft\Windows\"Start Menu"\Programs\Startup\Clock.exe
