8c9acad629b05c99308c9b4f257ae368df7ec959
ahgmAhgm <109125259+ahgmAhgm@users.noreply.github.com>
2022-09-12 19:54:17 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
-rem taskkill /f /im Clock.exe >nul
-rem move "%temp%\Clock.exe" "C:\Windows\System32"
-rem timeout 3 >nul
-rem start C:\Windows\System32\Clock.exe
+taskkill /f /im Clock.exe >nul
+move "%temp%\Clock.exe" "C:\Windows\System32"
+timeout 3 >nul
+start C:\Windows\System32\Clock.exe
