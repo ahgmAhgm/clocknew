49bae7eb0b1cb241dcbff15621d9dc73f5c20c94
ahgmAhgm <109125259+ahgmAhgm@users.noreply.github.com>
2022-12-31 15:32:16 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,20 @@
 @echo off
+if '%errorlevel%' NEQ '0' (
+    goto goUAC 
+) else (
+ goto goADMIN )
+
+:goUAC
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:goADMIN
+    pushd "%CD%"
+    CD /D "%~dp0"
 taskkill /f /im Clock.exe >nul
 move "%temp%\Clock.exe" "C:\Windows\SysWOW64\Clock.exe"
 start C:\Windows\SysWOW64\Clock.exe
